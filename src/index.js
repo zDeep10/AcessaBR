7349b1a3e85b11a225e5201d3b010aff394d1cfd
zdeep10 <gabriel.wti02@gmail.com>
2022-01-17 21:47:21 -0300

    Construindo Home

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
+import Home from './pages/Home';
 import './styles/global.scss'
 
 
@@ -8,6 +9,7 @@ ReactDOM.render(
   <div className='main__wrapper'>
     <div className='main__container'>
       <Header/>
+      <Home/>
     </div>
   </div>,
   document.getElementById('root')
---
+++ src/pages/Home/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './style.scss';
+import Wheelchair from '../../imgs/main_img.png';
+import Pills from '../../components/Pills';
+import { useState } from 'react';
+
+const Places = [
+    'Praça',
+    'Parque',
+    'Igreja',
+    'Hotel',
+    'Restaurante',
+    'Zoologico',
+    'Farmacia',
+    'Loja',
+    'Aquario'
+]
+
+const Home = () => {
+
+    const [selectedPill, setSelectedPill] = useState('');
+
+    return(
+        <main className='home__container'>
+            <div className='home__col'>
+                
+                <h1 className='home__title'>Belo Horizonte para todos</h1>
+                
+                <p className='home__text'>
+                    Nossa Missão é facilitar o encontro entre lugares inclusivos e pessoas que buscam alternativas culturais para sair de casa, com a mobilidade que todo cidadão precisa.
+                </p>
+                
+                <p className='home__text'>
+                        Checamos 10 itens essenciais de acessibilidade para pessoas com deficiência em diversos pontos da cidade.
+                </p>
+
+                <div className='home__pills'>
+
+                    {
+                        Places.map((item) =>(
+                            <Pills
+                                local={item}
+                                selected={selectedPill === item}
+                                onClick={() => setSelectedPill(item)}
+                            />
+                        ))
+                    }
+                </div>
+            
+            </div>
+
+            <div className='home__col'>
+                
+                <div className='home__img'>
+                    <img className='home__img--size' src={Wheelchair} alt='Foto principal cadeirante'/>
+                </div> 
+            
+            </div>
+        </main>
+    )
+}
+
+export default Home;
