02288b22670aa52f6532683442493c94d67d1071
zdeep10 <gabriel.wti02@gmail.com>
2022-01-19 15:45:21 -0300

    organizando codigo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from './components/Header';
-import Home from './pages/Home';
-import './styles/global.scss'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import "./styles/global.scss";
 
 ReactDOM.render(
-  <div className='main__wrapper'>
-    <div className='main__container'>
-      <Header/>
-      <Home/>
+  <div className="main__wrapper">
+    <div className="main__container">
+      <Header />
+      <Home />
     </div>
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
